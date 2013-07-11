bb59afeb4b3d99a4dfe9d498f47cafa07dcb8fc1
Jenna <jennabantjes@gmail.com>
2013-07-11 11:24:16 -0400

    Fix card type image fade

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,4 +1,7 @@
 ;(function($){
+    "use strict";
+
+  // Find Card Type
 
   $('#cardnumber').blur(function(){
 
@@ -6,26 +9,30 @@
 
       if (/^3[47][0-9]{13}$/.test(value))
       {
-        $('#amex').fadeIn(100, 0.8);
+        $('#amex').fadeTo(100, 1);
         $('#visa, #mastercard, #discover').fadeTo(300, 0.5);
+        $('.security-image').removeClass('regular').addClass('amex');
       }
 
       else if(/^4[0-9]{12}(?:[0-9]{3})?$/.test(value))
       {
-        $('#visa').fadeIn(100, 1);
+        $('#visa').fadeTo(100, 1);
         $('#amex, #mastercard, #discover').fadeTo(300, 0.5);
+        $('.security-image').removeClass('amex').addClass('regular');
       }
 
       else if(/^5[1-5][0-9]{14}$/.test(value))
       {
-        $('#mastercard').fadeIn(100, 1);
+        $('#mastercard').fadeTo(100, 1);
         $('#visa, #amex, #discover').fadeTo(300, 0.5);
+        $('.security-image').removeClass('amex').addClass('regular');
       }
 
       else if(/^6(?:011|5[0-9]{2})[0-9]{12}$/.test(value))
       {
-        $('#discover').fadeIn(100, 1);
+        $('#discover').fadeTo(100, 1);
         $('#visa, #mastercard, #amex').fadeTo(300, 0.5);
+        $('.security-image').removeClass('amex').addClass('regular');
       }
 
       else
@@ -34,11 +41,7 @@
       }
   });
 
-})(jQuery);
-
-
-;(function($){
-  "use strict";
+// Show/Hide Password
 
   $('#unmask').click(function(){
     if($('#unmask').is(':checked'))
