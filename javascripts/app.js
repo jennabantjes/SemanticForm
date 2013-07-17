14e3325451def6502c3c2931fb84becc025592cb
Jenna <jennabantjes@gmail.com>
2013-07-17 13:21:04 -0400

    Add validation images

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,17 +6,41 @@
     nope: 'javascripts/libs/jquery.validate.js'
   });
 
+// Show/Hide Password
+// Add dummy text field for password and add toggle link to form
+    $('input[type=password]').each(function(){
+      var el = $(this), elPH = el.attr("placeholder");
+      el.addClass("offPage").after('<input class="passText" placeholder="' + elPH + '" type="text" />');
+    });
+
+    //  keep password field and dummy text field in sync
+    $('input[type=password]').keyup(function(){
+      var elText = $(this).val();
+      $('.passText').val(elText);
+    });
+    $('.passText').keyup(function(){
+      var elText = $(this).val();
+      $('input[type=password]').val(elText);
+    });
+
+    // Toggle checkbox
+    $('#checkbox').click(function(e){
+      $('.show-password').toggleClass('unchecked');
+      $('input[type=password], .passText').toggleClass("offPage");
+      e.preventDefault(); // <-- prevent any default actions
+    });
+
   // Find Card Type
   function checkSecurityCode(){
           var code = $('.security').val().length;
 
           if(code === 3)
           {
-            $('.security').removeClass('invalid').addClass('valid');
+            $('.security-code').removeClass('invalid').addClass('valid');
           }
           else
           {
-            $('.security').removeClass('valid').addClass('invalid');
+            $('.security-code').removeClass('valid').addClass('invalid');
           }
   };
 
@@ -27,9 +51,10 @@
       // Amex
       if (/^3[47][0-9]{13}$/.test(value))
       {
-        $('#amex').fadeTo(100, 1);
-        $('#visa, #mastercard, #discover').fadeTo(300, 0.5);
+        $('#amex-image').fadeTo(100, 1);
+        $('#visa-image, #mastercard-image, #discover-image').fadeTo(200, 0.5);
         $('.security-image').removeClass('regular').addClass('amex');
+        $('.validation').removeClass('invalid').addClass('valid');
 
         $('.security').blur(function(){
             var code = $('.security').val().length;
@@ -48,9 +73,10 @@
       // Visa
       else if(/^4[0-9]{12}(?:[0-9]{3})?$/.test(value))
       {
-        $('#visa').fadeTo(100, 1);
-        $('#amex, #mastercard, #discover').fadeTo(300, 0.5);
+        $('#visa-image').fadeTo(100, 1);
+        $('#amex-image, #mastercard-image, #discover-image').fadeTo(200, 0.5);
         $('.security-image').removeClass('amex').addClass('regular');
+        $('.validation').removeClass('invalid').addClass('valid');
 
         $('.security').blur(checkSecurityCode);
       }
@@ -58,9 +84,10 @@
       // Mastercard
       else if(/^5[1-5][0-9]{14}$/.test(value))
       {
-        $('#mastercard').fadeTo(100, 1);
-        $('#visa, #amex, #discover').fadeTo(300, 0.5);
+        $('#mastercard-image').fadeTo(100, 1);
+        $('#visa-image, #amex-image, #discover-image').fadeTo(200, 0.5);
         $('.security-image').removeClass('amex').addClass('regular');
+        $('.validation').removeClass('invalid').addClass('valid');
 
         $('.security').blur(checkSecurityCode);
       }
@@ -68,57 +95,39 @@
       // Discover
       else if(/^6(?:011|5[0-9]{2})[0-9]{12}$/.test(value))
       {
-        $('#discover').fadeTo(100, 1);
-        $('#visa, #mastercard, #amex').fadeTo(300, 0.5);
+        $('#discover-image').fadeTo(100, 1);
+        $('#visa-image, #mastercard-image, #amex-image').fadeTo(200, 0.5);
         $('.security-image').removeClass('amex').addClass('regular');
+        $('.validation').removeClass('invalid').addClass('valid');
 
         $('.security').blur(checkSecurityCode);
       }
 
       else
       {
-        $('#cardnumber').val('Sorry, but that number is invalid');
+        $('.validation').removeClass('valid').addClass('invalid');
       }
   });
 
-/*  // Show/Hide Password
-  $('#checkbox').click(function(){
-    if($('#checkbox').is(':checked'))
-    {
-      $('.form').find('input:password').each(function(){
-        $("<input type='text' />").attr({name: this.name, value: this.value}).insertBefore(this);
-      }).remove();
-    }
-
-    else
-    {
-      $('.form').find('input:text').each(function(){
-        $("<input type='password' />").attr({name: this.name, value: this.value}).insertBefore(this);
-      }).remove();
-    }
-  });*/
-
-// ----- Setup: Add dummy text field for password and add toggle link to form; "offPage" class moves element off-screen
-    $('input[type=password]').each(function(){
-      var el = $(this), elPH = el.attr("placeholder");
-      el.addClass("offPage").after('<input class="passText" placeholder="' + elPH + '" type="text" />');
+/*// Fade image on click
+    $('#visa-image').click(function(){
+      $(this).fadeTo(100, 1);
+      $('#mastercard-image, #amex-image, #discover-image').fadeTo(300, 0.5);
     });
 
-    // ----- keep password field and dummy text field in sync
-    $('input[type=password]').keyup(function(){
-      var elText = $(this).val();
-      $('.passText').val(elText);
-    });
-    $('.passText').keyup(function(){
-      var elText = $(this).val();
-      $('input[type=password]').val(elText);
+    $('#mastercard-image').click(function(){
+      $(this).fadeTo(100, 1);
+      $('#visa-image, #amex-image, #discover-image').fadeTo(300, 0.5);
     });
 
-    // ----- Toggle link functionality - turn on/off "offPage" class on fields
-    $('#checkbox').click(function(e){
-      $('.show-password').toggleClass('unchecked');
-      $('input[type=password], .passText').toggleClass("offPage");
-      e.preventDefault(); // <-- prevent any default actions
+    $('#amex-image').click(function(){
+      $(this).fadeTo(100, 1);
+      $('#mastercard-image, #visa-image, #discover-image').fadeTo(300, 0.5);
     });
+
+    $('#discover-image').click(function(){
+      $(this).fadeTo(100, 1);
+      $('#mastercard-image, #amex-image, #visa-image').fadeTo(300, 0.5);
+    });*/
 
 })(jQuery);
